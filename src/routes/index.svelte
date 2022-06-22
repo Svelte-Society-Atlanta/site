ea1ed933c0417e7596ccd2aa8c2f8ecdc44925a9
Patrick Hultquist <patrick@monogram.io>
2022-06-21 22:35:11 -0400

    Initial commit 🥂

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Svelte Society ATL</title>
+</svelte:head>
+
+<div class="w-full h-full p-10 prose">
+	<h1 class="text-2xl font-bold">Svelte Society ATL</h1>
+	<p>Welcome to Svelte Society ATL. We're just getting started, so stay tuned.</p>
+	<p>
+		Really, we're in need of your help. Please <a href="mailto:svelte@patrick.app">reach out</a> if you
+		have ideas, resources, or time to grow Atlanta's Svelte community.
+	</p>
+</div>
